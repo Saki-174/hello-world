a74cde52363078ffc37b8b3b6806427c8e8727dd
Saki-174 <18381601561@163.com>
2025-09-06 09:48:44 +0800

    基本玩家移动和资源添加

---
+++ hello world/Assets/Script/Player/PlayerMove.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    /// <summary>
+    /// 玩家的基本移动
+    /// </summary>
+    private Rigidbody2D rb;//通过刚体控制玩家移动
+    [SerializeField] private float moveSpeed;//左右移动速度
+    [SerializeField] private float jumpSpeed;//跳跃速度
+    private float moveController;//设置unity提供预设方式移动
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    //控制玩家的移动函数（AD）
+    public void Move()
+    {
+        moveController = Input.GetAxisRaw("Horizontal");//水平输入移速
+        rb.velocity = new Vector2(moveSpeed * moveController, rb.velocity.y);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x,jumpSpeed);
+        }
+
+    }
+}
