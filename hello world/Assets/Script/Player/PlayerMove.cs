c0ec0fc95fad843876955ff4d94aeb763e77e14b
Saki-174 <18381601561@163.com>
2025-09-06 17:49:32 +0800

    加入地图

--- hello world/Assets/Script/Player/PlayerMove.cs
+++ hello world/Assets/Script/Player/PlayerMove.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class PlayerMove : MonoBehaviour
     /// </summary>
     private Rigidbody2D rb;//通过刚体控制玩家移动
     [SerializeField] private float moveSpeed;//左右移动速度
-    [SerializeField] private float jumpSpeed;//跳跃速度
+    [SerializeField] private float jumpSpeed;//跳跃速度    
     private float moveController;//设置unity提供预设方式移动
     // Start is called before the first frame update
     void Start()
@@ -22,16 +23,30 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         Move();
+        Turn();
     }
     //控制玩家的移动函数（AD）
     public void Move()
     {
         moveController = Input.GetAxisRaw("Horizontal");//水平输入移速
         rb.velocity = new Vector2(moveSpeed * moveController, rb.velocity.y);
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) )
         {
             rb.velocity = new Vector2(rb.velocity.x,jumpSpeed);
         }
 
     }
+    //控制玩家图层转向
+    public void Turn()
+    {
+        if(rb.velocity.x > 0)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+        if(rb.velocity.x < 0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+    }
+    
 }
