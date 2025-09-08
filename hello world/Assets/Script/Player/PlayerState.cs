4aeb90ce3a05a84e2bd6f44df18cb373ad8fca32
Saki-174 <18381601561@163.com>
2025-09-08 10:06:28 +0800

    aaa

---
+++ hello world/Assets/Script/Player/PlayerState.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public static PlayerState instance {  get; private set; } //单例模式用来确保角色只有一个状态
+    public static PlayerState Instance
+    {
+        get
+        {
+            //确保只有一个实例
+            if(instance == null)
+                instance = new PlayerState();
+            return instance;
+        }
+    }
+    enum State
+    {
+        idle,
+        move,
+        jump,
+        doubleJump,
+        fall,
+    }
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        State state = State.idle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
