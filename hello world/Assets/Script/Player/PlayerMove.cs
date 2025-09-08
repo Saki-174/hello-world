9664116d11f856496a601b7643d6863e93f7686b
Saki-174 <18381601561@163.com>
2025-09-08 09:25:15 +0800

    二段跳

--- hello world/Assets/Script/Player/PlayerMove.cs
+++ hello world/Assets/Script/Player/PlayerMove.cs
@@ -11,7 +11,8 @@ public class PlayerMove : MonoBehaviour
     /// </summary>
     private Rigidbody2D rb;//通过刚体控制玩家移动
     [SerializeField] private float moveSpeed;//左右移动速度
-    [SerializeField] private float jumpSpeed;//跳跃速度
+    [SerializeField] private float jumpSpeed_1;//跳跃速度
+    [SerializeField] private float jumpSpeed_2;//二段跳跃速度
     [SerializeField] private float jumpTimeController;//控制跳跃加速最大时间
     [SerializeField] private float upPower;//控制跳跃加速最大时间
     [SerializeField] private float downPower;//控制跳跃加速最大时间
@@ -19,6 +20,7 @@ public class PlayerMove : MonoBehaviour
     private float moveController;//设置unity提供预设方式移动
     private bool isOnGround;//人物是否在地面上，用于跳跃判断
     private bool isJumping;//人物是否正在跳跃，用于跳跃重力控制和二段跳
+    private bool doubleJump;//二段跳
     
     // Start is called before the first frame update
     void Start()
@@ -45,14 +47,23 @@ public class PlayerMove : MonoBehaviour
         //按住space
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed_1);
             isJumping = true;//正在跳跃
             jumpTime = 0;
+            doubleJump = true;
         }
         //松开space
         if (Input.GetKeyUp(KeyCode.Space))
         {
             isJumping = false;//跳跃结束 
+        }
+        //二段跳
+        if(doubleJump &&!isOnGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed_2);
+            isJumping = true;//正在跳跃
+            jumpTime = 0;
+            doubleJump = false;
         }
         //上升
         if (isJumping)
