f5ff20ea62e729ccc6411b92c8f3a5ef5f6e32e3
Saki-174 <18381601561@163.com>
2025-09-16 01:14:33 +0800

    结算星星

--- hello world/Assets/Script/UI/Scene/End.cs
+++ hello world/Assets/Script/UI/Scene/End.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class End : MonoBehaviour
 {
+    public Score s;//获取分数
+    public float fullScore;//满分
+    public List<GameObject> gameObjects = new List<GameObject>();//星星结算
     public static bool isVectory;//胜利
     public GameObject victoryPanel;//胜利结算动画
     private Animator animator;//获取动画组件
@@ -27,19 +30,39 @@ public class End : MonoBehaviour
             animator.SetBool("Victory", true);
             player.GetComponent<Animator>().SetBool("IsVictory", true);
             isVectory = true;
-            StartCoroutine(Good());//使用协程，胜利后一秒呼出胜利面板
+            StartCoroutine(Good());//使用协程，胜利后一秒呼出胜利面板           
+            
         }   
     }
     IEnumerator Good()
     {
         yield return new WaitForSeconds(1.5f);
         Victory();
+        if (s.score > fullScore / 3)
+        {
+            gameObjects[0].SetActive(true);
+            gameObjects[0].GetComponent<Animator>().SetBool("First",true);
+        }
+        yield return new WaitForSeconds(1f); 
+        if (s.score > (fullScore / 3) * 2)
+        {
+            gameObjects[1].SetActive(true);
+            gameObjects[1].GetComponent<Animator>().SetBool("Second", true);
+        }
+        yield return new WaitForSeconds(1f);
+        if (s.score == fullScore)
+        {
+            gameObjects[2].SetActive(true);
+            gameObjects[2].GetComponent<Animator>().SetBool("Third", true);
+        }       
+        yield return new WaitForSeconds(2f);
+        Time.timeScale = 0f;
     }
+    
     public void Victory()
     {
         audioSource.Play();
         victoryPanel.SetActive(true);
-        Time.timeScale = 0f;
     }
     public void Next()
     {
